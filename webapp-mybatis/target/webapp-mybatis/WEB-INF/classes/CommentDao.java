6885e3a3f9a3d9d714adb22f45ecb9f76eab5a6b
CodeHandPuppet <3216434441@qq.com>
2024-06-18 23:23:54 +0800

    1

--- webapp-mybatis/target/webapp-mybatis/WEB-INF/classes/CommentDao.java
+++
@@ -1,9 +0,0 @@
-package com.lxj.chatForum.dao;
-
-import com.lxj.chatForum.pojo.CommentPojo;
-
-import java.util.List;
-
-public interface CommentDao {
-    List<CommentPojo> getCommentsByForumId(String forumId);
-}
